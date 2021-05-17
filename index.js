d5a9e89dd5e5cf4b301b472c44b5f7d6c978b751
renananiceto <renan.a.ferreira94@gmail.com>
2021-05-17 10:32:43 -0300

    first comment

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+let idade = 26
+
+if(idade >=18 ){
+    console.log('Maior de idade')
+}
+
+// 2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+let souhuman = true
+if(idade >= 18 && souhuman == true){
+    console.log('parabéns humano')
+}
+// 3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+const aniversario = 'setembro'
+if( aniversario == 'janeiro' || aniversario == 'dezembro'){
+    console.log("parabens")
+}else{
+    console.log('"Hoje não"')
+}
+
+// *** Exercícios bônus
+// 4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+// 5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+
+
